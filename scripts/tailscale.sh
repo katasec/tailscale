44fe650f4aca8631ab50faded02c8c97252f9beb
Ameer Deen <ameer.deen@katasec.com>
2022-07-20 13:17:14 +0400

    Addin more options

--- scripts/tailscale.sh
+++ scripts/tailscale.sh
@@ -1,23 +1,79 @@
 #!/usr/bin/env bash
 
-# Check auth key was passed
-env
-if [ -z "$KEY" ]
-then
-      echo "Environment Variable \$KEY is empty, exitting!"
-      exit 1
-fi
+# Global vars
+myCmd=""
 
-# Configure User mode networking for tailscale daemon
+# ----------------------------------------------------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------------------------------------------------
+
+function base_command() {
+    # Check auth key was passed
+    if [ -z "$KEY" ]
+    then
+        echo "Environment Variable \$KEY is empty, exitting!"
+        exit 1
+    else
+        myCmd="tailscale up --authkey=$KEY"
+    fi
+}
+
+function add_exit_node() {
+    # Check auth key was passed
+    if [ -z "$EXIT_NODE" ]
+    then
+        echo "Exit Node was not requested and will not be configured."
+        EXIT_NODE=$false
+    fi
+
+    # If exit node if requested, add to command
+    if [ "$EXIT_NODE" = true ]
+    then
+        echo "Configuring as an exit node"
+        myCmd+=" --advertise-exit-node"
+    fi
+}
+
+function add_avertise_routes() {
+    if [ -z "$ADVERTISE_ROUTES" ]; 
+    then 
+        echo "No routes were requested to be advertised"; 
+    else 
+        echo "Will addvertise the provided routes in env var ADVERTISE_ROUTES: $ADVERTISE_ROUTES"
+        myCmd+=" --advertise-routes=$ADVERTISE_ROUTES"
+    fi
+
+}
+
+# ----------------------------------------------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------------------------------------------
+
+# Create base command
+base_command
+
+# Copgiure Exit Node
+add_exit_node
+
+# Configure advertise_routes
+add_avertise_routes
+
+
+# Start tailscale daemon with user mode networking
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
-# Start tailscale and advertise as exit node
-tailscale up --authkey=$KEY --advertise-exit-node
+# Output and Run tailscale command
+echo $myCmd
+eval $myCmd
 
 
-# Inifinite loop
-while true; 
-do 
-    echo "$(date), Sleeping 30 seconds..."
-    sleep 30
-done
+
+# tailscale up --authkey=$KEY --advertise-exit-node
+
+
+# # Inifinite loop
+# while true; 
+# do 
+#     echo "$(date), Sleeping 30 seconds..."
+#     sleep 30
+# done
